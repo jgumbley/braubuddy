4675addc35aba0f606087ec695c2d35ea1c570db
Jim Gumbley <jgumbley@thoughtworks.com>
2016-11-05 21:28:28 +0000

    use internalised config file

--- braubuddy/__init__.py
+++ braubuddy/__init__.py
@@ -1,57 +1,15 @@
 import os
-import shutil
-from pkg_resources import Requirement, resource_filename
-from xdg import BaseDirectory
 from . import thermostat
 from . import thermometer
 from . import envcontroller
 from . import output
 from . import apps
 
-
-def _deploy_config_file(destination_dir):
-    """
-    Copy the default Braubuddy config file to the given destnation directory.
-
-    :param destination_dir: Config file destination dir.
-    :type destination_dir: :class:`unicode`
-    :returns: Path to deployed config file.
-    :rtype: :class:`unicode`
-    """
-
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
-    config_file_path = os.path.join(destination_dir, CONFIG_FILENAME_BRAUBUDDY)
-    print('Copying default config file to {0}'.format(config_file_path))
-    default_config_path = os.path.join(CONFIG_DIR, CONFIG_FILENAME_BRAUBUDDY)
-    shutil.copy(default_config_path, config_file_path)
-    return config_file_path
-
-
 def _get_config_file_location():
     """
-    Locate a Braubuddy config file in the locations defined by the XDG spec.
-
-    If no config file exists, deploy the default Braubuddy config file to the
-    XDG config dir in the user's home dir ('~/.config/braubuddy/').
-
-    :returns: Path to config file.
-    :rtype: :class:`unicode`
+    Locate a Braubuddy config file
     """
-
-    for config_path in BaseDirectory.load_config_paths('braubuddy'):
-        config_file_path = os.path.join(
-            config_path, CONFIG_FILENAME_BRAUBUDDY)
-        if os.path.isfile(config_file_path):
-            break
-    else:
-        # No config file found. So deploy default config file to XDG config
-        # dir in user home dir.
-        config_path = os.path.join(
-            BaseDirectory.xdg_config_home, 'braubuddy')
-        config_file_path = _deploy_config_file(config_path)
-    print('Loading config from {0}'.format(config_file_path))
-    return config_file_path
+    return os.path.join(CONFIG_DIR, CONFIG_FILENAME_BRAUBUDDY)
 
 # Base dirs
 THIS_DIR = os.path.join(os.getcwd(), 'braubuddy')
