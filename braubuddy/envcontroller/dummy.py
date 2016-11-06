b77f1830789b0c147fdb4f17f78efa9d5d58adc4
Jim Gumbley <jgumbley@thoughtworks.com>
2016-11-06 12:33:45 +0000

    change to switch on and off

--- braubuddy/envcontroller/dummy.py
+++ braubuddy/envcontroller/dummy.py
@@ -12,18 +12,22 @@ class RelayHeaterController(IEnvController):
         self._heater_percent = 0
         self._cooler_percent = 0
 
+        self._devices = relayctl.connect()
+        self._dev = self.devices[0]
+
     def _relayset(self, bool):
-        devices = relayctl.connect()
-        dev = devices[0]
-        relayctl.switchon(dev, 1)
+
+        if bool:
+            relayctl.switchon(self._dev, 1)
+        else:
+            relayctl.switchoff(self._dev, 1)
 
     def set_heater_level(self, percent):
-
         if percent not in list(range(0, 101)):
             msg = '{0} is not in range 0-100'.format(percent)
             raise PercentageError(msg)
         print("heater set to {0}".format(percent))
-        self._relayset(True)
+        self._relayset(percent)
         self._heater_percent = percent
 
     def set_cooler_level(self, percent):
